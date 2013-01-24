6ce54ef561561e25d2598c69d6d386d69f1dbf95
Phil James-Roxby <phil.jamesroxby@gmail.com>
2013-01-23 19:14:22 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root :to => "static_pages#home"
 
   match '/help' => 'static_pages#help'
   match '/about' => 'static_pages#about'
   match '/contact' =>  'static_pages#contact'
-
+  match '/signup' => 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
