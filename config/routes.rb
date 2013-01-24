0afd797de69e0e00668d23b627728ba09b8b8afa
Phil James-Roxby <phil.jamesroxby@gmail.com>
2013-01-23 18:51:10 -0700

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  root :to => "static_pages#home"
 
-  get "static_pages/help"
-
-  get "static_pages/about"
+  match '/help' => 'static_pages#help'
+  match '/about' => 'static_pages#about'
+  match '/contact' =>  'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
